67c66b419ea40b75d8d4545797aa481b212286f5
katongole <katsisaac50@gmail.com>
2018-10-08 11:25:01 +0200

    The publicPath will be used within our server script as well in order to make sure files are served correctly on http://localhost:3000

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -8,9 +9,15 @@ module.exports = {
            },
   output: {
     filename: '[name].bundle.js',
+    publicPath: '/',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'inline-source-map',
+  devServer: {
+         contentBase: './dist'
+       },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
          new HtmlWebpackPlugin({
            title: 'Output Management'
          })
